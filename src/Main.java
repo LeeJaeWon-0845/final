729f077bc0b4030421ec5d315b1a520a10606d85
Your Name <you@example.com>
2023-11-07 20:10:08 +0900

    final

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world123123!");
-        System.out.println("Hello world123123!");
-        System.out.println("Hello world123123!");
-        System.out.println("Hello world123123!");
         System.out.println("final");
-        System.out.println("Hello world123123!");
-        System.out.println("Hello world123123!");
-        System.out.println("final");
-        System.out.println("final");
-        System.out.println("Hello world123123!");
-        System.out.println("Hello world123123!");
     }
 }
